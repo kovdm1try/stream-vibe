51481f677c74e1040967e9790d88316663b0108b
kovdm1try <kovtunov.04@icloud.com>
2025-08-31 15:00:15 +1000

    add css variables, utils classes and global styles

--- src/styles/index.js
+++ src/styles/index.js
@@ -3,3 +3,11 @@ import '@a1rth/css-normalize'
 
 // Подключение шрифтов
 import './fonts.scss'
+
+// Подключение переменных
+import './variables.scss'
+
+// подключение утилитарных классов
+import './utils.scss'
+
+import './globals.scss'
