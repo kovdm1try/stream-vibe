9f5955a5a9d518dda1e29994924a4e6a8ca235b6
kovdm1try <kovtunov.04@icloud.com>
2025-09-01 18:00:21 +1000

    complete header navbar

--- src/global.jsx
+++ src/global.jsx
@@ -5,7 +5,11 @@ import Footer from "@/layouts/Footer";
 import Content from "@/layouts/Content";
 
 export default function (props) {
-    const {children, title} = props;
+    const {
+        children,
+        title,
+        url, // адрес текущей страницы
+    } = props;
 
     return (
         <>
@@ -19,7 +23,7 @@ export default function (props) {
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
                 <link rel="manifest" href="/site.webmanifest"/>
             </Head>
-            <Header/>
+            <Header url={url}/>
 
             <Content>
                 {children}
--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,11 +1,54 @@
 import "./Header.scss"
 import Logo from "@/components/Logo";
+import classNames from "classnames";
 
-export default () => {
+export default (props) => {
+    const {
+        url,
+    } = props;
+
+    const menuItems = [
+        {
+            label: "Home",
+            href: '/'
+        },
+        {
+            label: "Movies & Shows",
+            href: '/movies'
+        },
+        {
+            label: "Support",
+            href: '/support'
+        },
+        {
+            label: "Subscriptions",
+            href: '/subscriptions'
+        },
+    ]
+
     return (
         <header className="header">
             <div className="header__inner container">
-                <Logo loading={'eager'} className={'header__logo'}/>
+                <Logo
+                    loading={'eager'}
+                    className={'header__logo'}
+                />
+                <nav className="header__menu">
+                    <ul className="header__menu-list">
+                        {menuItems.map(({label, href}, index) => (
+                            <li
+                                className="header__menu-item"
+                                key={index}
+                            >
+                                <a href={href} className={classNames('header__menu-link', {
+                                    'is-active': href === url // то что будет иметь true конкактинируется
+                                })}>
+                                    {label}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
             </div>
         </header>
     )
