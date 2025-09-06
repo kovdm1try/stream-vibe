313c082246c90433b7a80bedea7bcffcf36f5479
kovdm1try <kovtunov.04@icloud.com>
2025-09-06 13:31:57 +1000

    add BurgerButton dialog window

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -30,42 +30,44 @@ export default (props) => {
         <header className="header">
             <div className="header__inner container">
                 <Logo loading={"eager"} className={"header__logo"}/>
-                <nav className="header__menu">
-                    <ul className="header__menu-list">
-                        {menuItems.map(({label, href}, index) => (
-                            <li className="header__menu-item" key={index}>
-                                <a
-                                    href={href}
-                                    className={classNames("header__menu-link", {
-                                        "is-active": href === url, // то что будет иметь true конкактинируется
-                                    })}
-                                >
-                                    {label}
-                                </a>
-                            </li>
-                        ))}
-                    </ul>
-                </nav>
-                <div className="header__actions">
-                    <Button
-                        className="header__button"
-                        label="Search"
-                        isLabelHidden
-                        mode='transparent'
-                        iconName={'search'}
-                    >
-                    </Button>
-                    <Button
-                        className="header__button"
-                        label='Notifications'
-                        isLabelHidden
-                        mode='transparent'
-                        iconName={'notification'}
-                    >
-                    </Button>
-                </div>
+                <dialog className="header__overlay-menu-dialog">
+                    <nav className="header__menu">
+                        <ul className="header__menu-list">
+                            {menuItems.map(({label, href}, index) => (
+                                <li className="header__menu-item" key={index}>
+                                    <a
+                                        href={href}
+                                        className={classNames("header__menu-link", {
+                                            "is-active": href === url, // то что будет иметь true конкактинируется
+                                        })}
+                                    >
+                                        {label}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </nav>
+                    <div className="header__actions">
+                        <Button
+                            className="header__button"
+                            label="Search"
+                            isLabelHidden
+                            mode='transparent'
+                            iconName={'search'}
+                        >
+                        </Button>
+                        <Button
+                            className="header__button"
+                            label='Notifications'
+                            isLabelHidden
+                            mode='transparent'
+                            iconName={'notification'}
+                        >
+                        </Button>
+                    </div>
+                </dialog>
                 <BurgerButton
-                    className={"header__burger-button"}
+                    className={"header__burger-button visible-table"}
                 />
             </div>
         </header>
